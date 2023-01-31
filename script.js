13089b7d5e0849148e9ec344ef3b0676c1353de9
ashan <indrajee79@gmail.com>
2023-01-31 19:16:52 +0530

    js stuff

--- script.js
+++ script.js
@@ -8,14 +8,18 @@ const displayProducts = () => {
         return;
     }
 
-    productsContainer.innerHTML = filteredProducts.map(product) => {
+    productsContainer.innerHTML = filteredProducts.map((product) => {
         const { id, title, image, price } =  product;
         return `<article class="product" data-id="${id}">
-            <img src="${image}"
-            class="product-img img"
-            alt=""/>
-            <footer>
-                <h5 class="">
-            `
-    }
+                <img src="${image}"
+                class="product-img img"
+                alt=""/>
+                <footer>
+                    <h5 class="product-name">${title}</h5>
+                    <span class="product-price">${price}</span>
+                </footer>
+                </article>`;
+
+    })
+    .join('');
 }
